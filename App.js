84efad8f9c1412abf3a25e44b6a07d6217c9fe5a
Dhiraj Pradhan <dhiraj.pradhan@amniltech.com>
2023-08-29 00:07:11 +0545

    dimension API usage

--- App.js
+++ App.js
@@ -1,58 +1,35 @@
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, Dimensions} from "react-native";
 
 export default function App(){
     return  (
         <View style={styles.container}>
-            <View style={styles.darkMode}>
-                <Text style={styles.darkModeText}>
-                    StyleSheet Inheritance Example
-                    <Text style={styles.boldText}>Text in bold.</Text>
-                </Text>
-            </View>
-            <Text style={styles.title}>Stylesheet API</Text>
-            <View style={[styles.box, styles.lightblueBg, styles.androidShadow]}>
-                <Text>LightBlue Box</Text>
-            </View>
-            <View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
-                <Text>LightGreen Box</Text>
+            <View style={styles.box}>
+                <Text style={styles.text}>Welcome !</Text>
             </View>
         </View>
     )
 }
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
-    container: {flex: 1, backgroundColor:"lightgray", padding: 60},
-    darkMode: {backgroundColor: "black"},
-    darkModeText: {color: "white"},
-    boldText: {fontWeight: "bold"},
-    title: {fontStyle: "italic", fontSize: 15},
-    box: {
-            height: 250,
-            width: 250,
-            paddingHorizontal: 50,
-            paddingVertical:25,
-            backgroundColor: "pink",
-            marginVertical: 10,
-            borderWidth: 2,
-            borderColor: "violet",
-            borderRadius:15,
-        },
-    lightblueBg: {backgroundColor: "lightblue"},
-    lightgreenBg: {backgroundColor: "lightgreen"},
-    //shadow property for ios
-    boxShadow: {
-        shadowColor: "blue",
-        shadowOffset: {
-            width: 6,
-            height: 6,
-        },
-        shadowOpacity: 0.6,
-        shadowRadius: 4
+    container: {
+        flex: 1,
+        backgroundColor:"plum",
+        // padding: 60,
+        alignItems: "center",
+        justifyContent: "center"
     },
-    //shadow property for android
-    androidShadow: {
-        elevation: 10,
-        shadowColor: "violet"
+    box:{
+        width: windowWidth > 500 ? '70%' :  "90%",
+        height: windowHeight > 600 ? '60%' : "90%",
+        backgroundColor: "lightblue",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    text: {
+        fontSize: windowWidth > 500 ? 50 : 24
     }
+
 });
