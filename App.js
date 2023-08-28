df5081a02a22d3250804b8f2a12dacefbcc98d9d
Dhiraj Pradhan <dhiraj.pradhan@amniltech.com>
2023-08-29 00:22:02 +0545

    Drawback of Dimension API and resolution using useState and useEffect

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import {View, Text, StyleSheet, Dimensions} from "react-native";
+import {useState, useEffect} from "react";
 
 export default function App(){
+
+    const [windowDimension, setWindowDimension] = useState({
+        window: Dimensions.get("window")
+    });
+
+    useEffect(() => {
+        const subscription = Dimensions.addEventListener("change", ({window}) => {
+            setWindowDimension({window});
+
+        })
+        return () => subscription?.remove();
+    });
+
+    const {window} = windowDimension;
+    const windowWidth = window.width;
+    const windowHeight = window.height;
+
     return  (
         <View style={styles.container}>
-            <View style={styles.box}>
-                <Text style={styles.text}>Welcome !</Text>
+            <View style={[
+                styles.box,
+                {
+                    width: windowWidth > 500 ? '70%' :  "90%",
+                    height: windowHeight > 600 ? '60%' : "90%",
+                }
+
+            ]}>
+                <Text style={{fontSize: windowWidth > 500 ? 50 : 24}}>Welcome !</Text>
             </View>
         </View>
     )
 }
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").height;
+//
+// console.log({windowWidth, windowHeight});
 
 const styles = StyleSheet.create({
     container: {
@@ -22,14 +49,14 @@ const styles = StyleSheet.create({
         justifyContent: "center"
     },
     box:{
-        width: windowWidth > 500 ? '70%' :  "90%",
-        height: windowHeight > 600 ? '60%' : "90%",
+        // width: windowWidth > 500 ? '70%' :  "90%",
+        // height: windowHeight > 600 ? '60%' : "90%",
         backgroundColor: "lightblue",
         alignItems: "center",
         justifyContent: "center"
     },
-    text: {
-        fontSize: windowWidth > 500 ? 50 : 24
-    }
+    // text: {
+    //     fontSize: windowWidth > 500 ? 50 : 24
+    // }
 
 });
